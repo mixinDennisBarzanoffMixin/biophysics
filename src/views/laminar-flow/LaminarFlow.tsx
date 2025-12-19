81fca40deb5c91907ebbd84139e1cd2c881d790a
mixinDennisBarzanoffMixin <102508565+mixinDennisBarzanoffMixin@users.noreply.github.com>
2025-12-19 11:26:10 +0000

    Cursor: Apply local changes for cloud agent

--- src/views/laminar-flow/LaminarFlow.tsx
+++ src/views/laminar-flow/LaminarFlow.tsx
@@ -1,4 +1,3 @@
-// ... existing code ...
 import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { KatexEquation } from '@/components/KatexEquation'
